251f1d81df0c2cda10e9e36f283b318f77fea1e9
natalielyn <nlmaxwell@gmail.com>
2020-03-01 22:49:03 -0800

    changed title

--- src/components/header/header.js
+++ src/components/header/header.js
@@ -5,7 +5,7 @@ class Header extends React.Component {
   render() {
     return (
       <header>
-        <h1 id="header">ToDo Application</h1>
+        <h1 id="header">To.Do Application</h1>
       </header>
     );
   }
